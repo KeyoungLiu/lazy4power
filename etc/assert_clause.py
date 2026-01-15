cccad8c4abf98052f881055b9790dfef659d3523
Keyoung <Keyoung.lau@gmail.com>
2026-01-15 11:32:56 +0800

    删文件

--- etc/assert_clause.py
+++
@@ -1,8 +0,0 @@
-try:
-    n=input("请输入一个数字:")
-    assert n.isdigit(),"只能输入数字"
-    print("你输入的是：",n)
-except Exception as ex:
-    print("发现错误:",ex)
-
-
--- etc/multiplication_table.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# 实现九九乘法表
-for i in range(1, 10):
-    for j in range(1, i + 1):
-        print(f"{j}*{i}={j*i}".ljust(7), end="")  # ljust(6)左对齐，长度为6，右补空格
-    print("")  # 打印一个空格
